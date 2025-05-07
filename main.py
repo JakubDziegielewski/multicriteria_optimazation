b7cba0fa7388712de73d411339d4003575f63b31
JakubDziegielewski <kubadziegiel1@gmail.com>
2025-05-07 14:53:41 +0200

    Added edge case for the same start and end node

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from src.network_creator import NetworkCreator
 from src.path import Path
-from numpy.random import seed
 
-network_creator = NetworkCreator("network_source\janos-us-ca")
+network_creator = NetworkCreator("network_source\janos-us-ca", random_seed=0)
 network = network_creator.create_physical_network()
 
-path = Path("Vancouver", "Dallas", network)
+path = Path("Montreal", "LosAngeles", network)
 for p in path.edge_path:
     print(p)
 print(f"Throughput: {path.throughput}, Delay: {path.delay}, Error rate: {path.error_rate}")
--- src/path.py
+++ src/path.py
@@ -4,10 +4,13 @@ class Path:
     def __init__(self, first_node_name: str, last_node_name: str, network: Network):
         self.first_node_name = first_node_name,
         self.last_node_name = last_node_name,
+        if last_node_name == first_node_name:
+            raise ValueError("Starting Node and Destination Node are the same")
         self.edge_path = self.depth_first_search(network, first_node_name, last_node_name)
         self.throughput = min([edge.throughput for edge in self.edge_path])
         self.delay = sum([edge.delay for edge in self.edge_path])
         self.error_rate = round(sum([edge.error_rate for edge in self.edge_path]), 3)
+        
     def depth_first_search(self, network: Network, first_node_name:str, last_node_name:str) -> None:
         next_node_name = first_node_name
         visited_nodes = [next_node_name]
